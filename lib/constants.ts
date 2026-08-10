78aa398afc595669e05a260ddc56494640add570
shubham-zibma <155353133+shubham-zibma@users.noreply.github.com>
2026-08-10 16:30:31 +0530

    Update Cloudinary credentials in lib/constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,8 +1,8 @@
 export const MONGODB_URI =
   'mongodb+srv://gautamtrading:gautamtrading@cluster0.jwprrpo.mongodb.net/gautamtrading?retryWrites=true&w=majority';
 
-export const CLOUDINARY_CLOUD_NAME = 'drx6ryufl';
-export const CLOUDINARY_API_KEY = '674271286781139';
-export const CLOUDINARY_API_SECRET = '-84ePPr7vERpPv-RDzRL0TtiJh0';
+export const CLOUDINARY_CLOUD_NAME = 'wvorocin';
+export const CLOUDINARY_API_KEY = '294823518775962';
+export const CLOUDINARY_API_SECRET = 'ELUDg-pzQVQimjKtUvoHKO5Gt6w';
 
 export const APP_URL = 'https://moxfood.com';
